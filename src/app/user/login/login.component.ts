27775b7f481067f11a095224b7ec61de1e430664
Luis R <jaskokoyn@gmail.com>
2024-07-07 17:08:48 -0400

    section 8

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  credentials = {
+    email: '',
+    password: '',
+  };
 
+  login() {
+    console.log(this.credentials);
+  }
 }
